18f7237ba758f8d9adfd11d67b3f874c868af39d
Johann <johann@immedia.co.za>
2013-03-25 08:16:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChooseTeams2::Application.config.secret_token = 'dd10bb044e437e4d07f0fea8971303f1b9e432c824fa355ab8bfed3c8912d52d509c7c85a2b6adf85d111aa6338df973dc18770c82158e76d50e674829d6afae'
